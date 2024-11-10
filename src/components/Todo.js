8e47f6476b521c7790fc70dc65e767e543f9b307
MayberryKory <102997911+MayberryKory@users.noreply.github.com>
2024-11-09 22:07:33 -0700

    Homework Complete

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Todo({ todo, index, toggleComplete, removeTodo }) {
+    return (
+        <div style={{ textDecoration: todo.completed ? 'line-through' : '' }}>
+            {todo.text}
+            <button onClick={() => toggleComplete(index)}>Complete</button>
+            <button onClick={() => removeTodo(index)}>Remove</button>
+        </div>
+    );
+}
+
+export default Todo;
